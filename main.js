68def16944a3f19777bdd87100ead24eda7a9b2a
Lucas <lucas.y.tan.life@outlook.com>
2024-01-19 23:21:33 +0800

    1-4 loader

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/**
+ * 第一次看到 CSS 被写在了 JavaScript 里！这其实都是 style-loader 的功劳，它的工作原理大概是把 CSS 内容用 JavaScript 里的字符串存储起来， 在网页执行 JavaScript 时通过 DOM 操作动态地往 HTML head 标签里插入 HTML style 标签。
+ */
+require('./main.css');
 // 通过 CommonJS 规范导入 show 函数
 const show = require('./show.js');
 // 执行 show 函数
