540a5e8f8f1b3b019f740ead0c14856a90c9ef28
Lucas <lucas.y.tan.life@outlook.com>
2024-01-19 23:54:51 +0800

    1-5 & 1-6, plugin and dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     // JavaScript 执行入口文件
     entry: './main.js',
+    mode: 'development',
+    /**
+     * webpack-dev-server 在编译之后不会写入任何输出文件，而是将 bundle 文件保留在内存中，然后将它们作为可访问资源部署在 server 中，就像是挂载在 server 根路径上的真实文件一样。
+     * 以下配置告知 webpack-dev-server 将 dist 目录下的文件作为可访问资源部署在 localhost:8080。。
+     */
+    devServer: {
+		static: "./"
+	},
     output: {
         // 把所有依赖的模块合并输出到一个 bundle.js 文件
         filename: 'bundle.js',
@@ -11,11 +20,12 @@ module.exports = {
     },
     module: {
         rules: [
-          {
-            // 用正则去匹配要用该 loader 转换的 CSS 文件
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-          }
+            {
+                // 用正则去匹配要用该 loader 转换的 CSS 文件
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }
         ]
-      }
+    },
+    plugins: [new MiniCssExtractPlugin()],
 };
